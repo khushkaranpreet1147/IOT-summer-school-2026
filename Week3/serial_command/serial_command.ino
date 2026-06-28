2b0af35fc37bde425cfbb9639dcdfc6f689ddc9e
khushkaranpreet1147 <k.kaur7814jeet@gmail.com>
2026-06-28 22:01:49 +0530

    feat: implement LED_ON and LED_OFF commands.

--- Week3/serial_command/serial_command.ino
+++ Week3/serial_command/serial_command.ino
@@ -5,5 +5,19 @@ void setup() {
 }
 
 void loop() {
+  
+  if (Serial.available() > 0) {
+    String command = Serial.readStringUntil('\n');
+    command.trim();
+
+    if (command == "LED_ON") {
+      digitalWrite(LED_BUILTIN, HIGH);
+      Serial.println("LED is ON");
+    } else if (command == "LED_OFF") {
+      digitalWrite(LED_BUILTIN, LOW);
+      Serial.println("LED is OFF");
+    }
+  }
+}
 }
 
