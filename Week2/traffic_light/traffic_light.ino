09d1645259723cb45625e8fa35cb5a2788b98f83
khushkaranpreet1147 <k.kaur7814jeet@gmail.com>
2026-06-28 19:38:53 +0530

    Update traffic_light.ino
    This program implements a robust traffic light control system on an Arduino UNO, featuring:
    
    ​Sequential Logic: A standard 3-LED cycle (Red, Yellow, Green) with precise timing intervals (5s, 2s, 4s).
    
    ​Interrupt-Ready Logic: A pedestrian-activated push button on Pin 7 that triggers an immediate 8-second "Red Light" safety hold.
    
    ​System Monitoring: Real-time logging of light states and timestamps via the Serial Monitor for debugging and performance tracking.

--- Week2/traffic_light/traffic_light.ino
+++ Week2/traffic_light/traffic_light.ino
@@ -1,1 +1,40 @@
+const int redLED=2;
+const int redLED=3;
+const int redLED=4;
+const int redLED=7;
+void setup() {
+  
+ pinMode(redLED, OUTPUT);
+ pinMode(yellowLED, OUTPUT);
+   pinMode(greenLED, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+  Serial.begin(9600);
+}
 
+void loop() {
+ 
+  if (digitalRead(buttonPin) == LOW) {
+    Serial.println("Pedestrian button pressed! Red light.");
+    digitalWrite(redLED, HIGH);
+    digitalWrite(yellowLED, LOW);
+    digitalWrite(greenLED, LOW);
+    Serial.print("Time: "); Serial.println(millis());
+    delay(8000); 
+    } 
+  else 
+  {
+   digitalWrite(redLED, HIGH); digitalWrite(yellowLED, LOW); digitalWrite(greenLED, LOW);
+    Serial.println("State: RED"); Serial.println(millis());
+    delay(5000);
+    
+   
+    digitalWrite(redLED, LOW); digitalWrite(yellowLED, HIGH); digitalWrite(greenLED, LOW);
+    Serial.println("State: YELLOW"); Serial.println(millis());
+    delay(2000);
+    
+    
+    digitalWrite(redLED, LOW); digitalWrite(yellowLED, LOW); digitalWrite(greenLED, HIGH);
+    Serial.println("State: GREEN"); Serial.println(millis());
+    delay(4000);
+  }
+}
