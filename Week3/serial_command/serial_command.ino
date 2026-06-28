e1aa9f876b16009a105ec1ddb8eaf831698f9aa3
khushkaranpreet1147 <k.kaur7814jeet@gmail.com>
2026-06-28 22:05:34 +0530

    feat: imlement BLINK_X, STATUS, and input validation.

--- Week3/serial_command/serial_command.ino
+++ Week3/serial_command/serial_command.ino
@@ -16,6 +16,26 @@ void loop() {
     } else if (command == "LED_OFF") {
       digitalWrite(LED_BUILTIN, LOW);
       Serial.println("LED is OFF");
+    } 
+    
+    else if (command.startsWith("BLINK_")) {
+      int count = command.substring(6).toInt();
+      if (count >= 1 && count <= 9) {
+        for (int i = 0; i < count; i++) {
+          digitalWrite(LED_BUILTIN, HIGH); delay(500);
+          digitalWrite(LED_BUILTIN, LOW); delay(500);
+        }
+        Serial.print("Blinked "); Serial.print(count); Serial.println(" times");
+      }
+    } 
+    
+    else if (command == "STATUS") {
+      Serial.print("Pin State (LED_BUILTIN): ");
+      Serial.println(digitalRead(LED_BUILTIN) == HIGH ? "ON" : "OFF");
+    } 
+    
+    else {
+      Serial.println("ERROR: Unknown command");
     }
   }
 }
