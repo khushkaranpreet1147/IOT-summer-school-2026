9fd62d7d91ae0e38b7d06704d9741db2240bba1c
khushkaranpreet1147 <k.kaur7814jeet@gmail.com>
2026-06-28 21:43:29 +0530

    feat: add chord substitute logic(play sol when 2+buttons pressed)

--- Week3/digital_piano/digital_piano.ino
+++ Week3/digital_piano/digital_piano.ino
@@ -14,14 +14,23 @@ void setup() {
 
 void loop() {
   
-  if (digitalRead(btnDo) == LOW) {
-    tone(buzzerPin, 262); // Do
+ void loop() {
+  int pressedCount = 0;
+  if (digitalRead(btnDo) == LOW) pressedCount++;
+  if (digitalRead(btnRe) == LOW) pressedCount++;
+  if (digitalRead(btnMi) == LOW) pressedCount++;
+  if (digitalRead(btnFa) == LOW) pressedCount++;
+
+  if (pressedCount >= 2) {
+    tone(buzzerPin, 392); // Sol (Chord Substitute)
+  } else if (digitalRead(btnDo) == LOW) {
+    tone(buzzerPin, 262);
   } else if (digitalRead(btnRe) == LOW) {
-    tone(buzzerPin, 294); // Re
+    tone(buzzerPin, 294);
   } else if (digitalRead(btnMi) == LOW) {
-    tone(buzzerPin, 330); // Mi
+    tone(buzzerPin, 330);
   } else if (digitalRead(btnFa) == LOW) {
-    tone(buzzerPin, 349); // Fa
+    tone(buzzerPin, 349);
   } else {
     noTone(buzzerPin);
   }
